c00000dfadc7d6f3f461a8084c3467f955dd7222
Thundercat <michael.c.haines@gmail.com>
2014-03-15 13:09:20 -0600

    Runs to completion

--- longway.py
+++ longway.py
@@ -4,13 +4,20 @@ import sys
 
 def divisors(n):
     divisors = []
+
     i = 1
     if n not in divisorHistory:
-        while i < n/i:
+        while i <= n/i:
             if n % i == 0:
-                divisors.extend([i,n/i])
+                if i == n/i:
+                    divisors.append(i)
+                else:
+                    divisors.extend([i,n/i])
             i += 1
     divisors.sort()
+    if len(divisors) == 2:
+        #n is prime
+        primes.append(n)
     divisorHistory[n] = divisors
 
     #new stuff
@@ -23,11 +30,14 @@ start = time.time()
 #out.sort()
 
 divisorHistory = {}
-z = 30 
+primes = []
+limit = 1000000
+z = limit + 1 
+print 'Finding divisors:  '
 while z > 0:
     divisors(z)
     sys.stdout.write('\r')
-    sys.stdout.write(str(z))
+    sys.stdout.write(' ' + str(100*(limit-z)/limit) + '%     ')
     sys.stdout.flush()
     z -= 1
 sys.stdout.write('\n')
@@ -36,9 +46,32 @@ divisorTotal = 0
 for key in divisorHistory:
    divisorTotal += sum(divisorHistory[key])
 
+primeGenerators = []
+progress = 1
+print 'Checking for prime generators'
+for n in divisorHistory:
+    sys.stdout.write('\r')
+    sys.stdout.write(' ' + str(100*progress/limit) + '%   ')
+    progress += 1
+    sys.stdout.flush()
+    candidate = True
+    #print 'Evaluating ' + str(n) + ' as primeGenerator candidate.'
+    for d in divisorHistory[n]:
+    #    print 'For divisor ' + str(d) + ' of generator candidate ' + str(n) + ':  d + n/d = ' + str(d + n/d)
+        if (d + n/d) not in primes:
+            #no longer a candidate
+            candidate = False
+    if candidate == True:
+        primeGenerators.append(n)
+
+sys.stdout.write('\n')
+
+if limit+1 in primeGenerators:
+    primeGenerators.remove(limit)
 stopTime = time.time()
 print divisorHistory
-print divisorTotal
-
-print 'Computation time: ' + str(stopTime-start)
-
+print 'Divisor total check: ' + str(divisorTotal)
+print 'Identified the following primes: ' + str(primes)
+print 'Prime Generators found: ' + str(primeGenerators)
+print 'Sum of prime generators: ' + str(sum(primeGenerators))
+print 'Computation time: ' + str(stopTime-start) 
