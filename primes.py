b781c727be64b0171ccf5b0b24884fc5d56f3982
Thundercat <michael.c.haines@gmail.com>
2014-03-15 19:03:27 -0600

    more efficient primes

--- primes.py
+++ primes.py
@@ -1,30 +1,42 @@
 import time
+import sys
 
 startTime = time.time()
 
 
 def sieve(n):
     #Return set of primes <= n
-    candidates =range(1,n+1)
-    i=1
+    candidates =range(0,n+1)
+    i=2
     prime = candidates[i]
+    sys.stdout.write('\n')
     while prime*prime <= n:
-        prime = candidates[i]
-        #print 'Removing multiples of ' + str(prime) + ' from candidates'
-        multiple = 2
-        candidates = set(candidates)
-        while prime*multiple <= n:
-            if prime*multiple in candidates:
-                candidates.remove(prime*multiple)
-            multiple += 1
-        candidates = list(candidates)
+        if candidates[i]:
+            prime = candidates[i]
+            sys.stdout.write('\r')
+            sys.stdout.write('Removing multiples of ' + str(prime) + ' from candidates:      ')
+            sys.stdout.flush()
+            
+            multiple = 2
+            while prime*multiple <= n:
+                if candidates[prime*multiple]:
+                    if 100*prime*multiple/n < 10:
+                        sys.stdout.write('\b\b')
+                    else:
+                        sys.stdout.write('\b\b\b')
+                    sys.stdout.write(str(100*prime*multiple/n) + '%')
+                    sys.stdout.flush()
+                    candidates[prime*multiple] = None
+                multiple += 1
         i += 1
-    return candidates
+    return filter(None,candidates)
 
 primes = []
-n = 1000000
+n = 100000000
 startTime = time.time()
-primes.extend(sieve(n))
+a=sieve(n)
+#print a
+#primes.extend(sieve(n))
 stopTime = time.time()
 #print primes
 print 'Time to compute primes < ' + str(n) + ' :   ' + str(stopTime-startTime)
